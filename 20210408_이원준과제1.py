2de4b2ec597a3da8f995fb2b9e4f78af685dd055
jlee0705 <81130257+bbeuro132@users.noreply.github.com>
2021-04-09 08:56:53 +0900

    Create 20210408_이원준과제1.py

---
+++ 20210408_이원준과제1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[49]:
+
+
+chSub = 'Duck'
+chHor = 'Gourd'
+chTra = 'Spitz'
+
+submarine = '%sy Mc%sface' % (chSub, chSub)
+racehorse = '%sy Mc%sface' % (chHor, chHor)
+train = '%sy Mc%sface' % (chTra, chTra)
+
+print(submarine.lower())
+print(racehorse.lower())
+print(train.lower())
+

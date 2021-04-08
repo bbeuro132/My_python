737c427718c4425640178a23231d6b98df60bdb5
jlee0705 <81130257+bbeuro132@users.noreply.github.com>
2021-04-09 08:56:57 +0900

    Create 20210408_이원준과제2.py

---
+++ 20210408_이원준과제2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+submarine = '{Boat}y Mc{Boat}face'.format(Boat='Duck')
+racehorse = '{Horse}y Mc{Horse}face'.format(Horse='Gourd')
+train = '{Train}y Mc{Train}face'.format(Train='spitz')
+
+print(submarine)
+print(racehorse)
+print(train)
+
